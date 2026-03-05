996ed374f7496b1ca0c7c0c692d825a2ec3e4b5f
script-VIP <imanfals511@gmail.com>
2026-03-05 14:04:34 +0700

    Update ecosystem.config.js

--- Botresseler/ecosystem.config.js
+++ Botresseler/ecosystem.config.js
@@ -2,14 +2,14 @@ module.exports = {
   apps: [{
     name: "botresseler",
     script: "app.js",
-    cwd: "/root/Botresseler",
+    cwd: "/root/bott/Botresseler",
     instances: 1,
     exec_mode: "fork",
     autorestart: true,
     watch: false,
     max_memory_restart: "500M",
-    error_file: "/root/.pm2/logs/botresseler-error.log",
-    out_file: "/root/.pm2/logs/botresseler-out.log",
+    error_file: "/root/bott/Botresseler/logs/error.log",
+    out_file: "/root/bott/Botresseler/logs/out.log",
     log_date_format: "YYYY-MM-DD HH:mm:ss"
   }]
 };
